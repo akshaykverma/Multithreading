6b5ef59a83b35eccaca8713a32299eeebedaf7fb
Akshay <akshay.kverma92@gmaiil.com>
2023-10-02 11:05:51 +0530

    adding PhilosopherThread

--- DiningPhilosopherProblem/src/com/multithreading/diningphilosopher/PhilosopherThread.java
+++ DiningPhilosopherProblem/src/com/multithreading/diningphilosopher/PhilosopherThread.java
@@ -1,5 +1,25 @@
 package com.multithreading.diningphilosopher;
 
-public class PhilosopherThread {
+import com.multithreading.diningphilosopher.models.Philosopher;
+
+public class PhilosopherThread implements Runnable {
+
+	private Philosopher philosopher;
+		
+	public PhilosopherThread(Philosopher philosopher) {
+		this.philosopher = philosopher;
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			philosopher.think();
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
